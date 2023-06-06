903411d6380ab2a902fefbfbdebb4844fc618bc8
Shin <ykingdev@gmail.com>
2023-06-06 12:48:06 +0200

    Clipped overflow

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function Increment() {
 
 function App() {
   return (
-    <div className="App bg-[#090909] h-screen flex flex-col justify-center w-screen  text-white">
+    <div className="overflow-clip bg-[#090909] h-screen flex flex-col justify-center w-screen  text-white">
       <Increment />
       <footer className="text-center text-[#969696] text-sm mt-5">
         <p>Developed by <a href="http://ykingdev.com/" className="text-[#969696] hover:text-[#090909]">YKingDev</a></p>
